39eff921c11cea9dc4a4fed224d3b02d800b7be3
Ckyle Pina <ckylepina123@gmail.com>
2025-01-26 22:37:02 -0800

    6

---
+++ green-horizons-app/app/Inventory/addbagsform/page.tsx
@@ -0,0 +1,13 @@
+// app/Inventory/addbagsform/page.tsx
+
+import React from 'react';
+
+const AddBagsForm: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default AddBagsForm;
---
+++ green-horizons-app/app/Inventory/page.tsx
@@ -0,0 +1,13 @@
+// app/Inventory/addbagsform/page.tsx
+
+import React from 'react';
+
+const Inventory: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Inventory;
